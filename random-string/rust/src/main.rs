00d22e6f86344547b9843e536867a282ea79e84e
Inside <inside@insidezhou.me>
2019-10-21 00:53:56 +0800

    improve random-string in rust

--- random-string/rust/src/main.rs
+++ random-string/rust/src/main.rs
@@ -6,25 +6,32 @@ use rand::{thread_rng, Rng};
 use std::env;
 use std::process;
 
+const DEFAULT_LEN: usize = 16;
+
 fn main() {
-    let mut num: usize = 16;
+    let mut len: usize = DEFAULT_LEN;
 
     match env::args().nth(1) {
         Some(arg) => match arg.parse::<usize>() {
-            Ok(n) => num = n,
+            Ok(arg_len) => len = arg_len,
             _ => {
-                eprintln!("(argument must be usize, got {})", arg);
+                eprintln!("(len must be usize, got {})", arg);
                 process::abort();
             }
         },
-        _ => println!("(using default n={})", num),
+        _ => println!("(using default len={})", DEFAULT_LEN),
+    }
+
+    if 0 == len {
+        len = DEFAULT_LEN;
+        println!("(got len=0, using default len={})", len)
     }
 
     let rand_string: String = thread_rng()
         .sample_iter(&Alphanumeric)
-        .skip(num)
-        .take(num)
+        .skip(len)
+        .take(len)
         .collect();
 
-    println!("{}", rand_string);
+    println!("{}", rand_string)
 }

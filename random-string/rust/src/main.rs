35f1ad1dac407d401f6537ca18018c781b2b6fa2
Inside <inside@insidezhou.me>
2019-10-22 21:36:27 +0800

    parse cli argument using clap
    random-string in rust

--- random-string/rust/src/main.rs
+++ random-string/rust/src/main.rs
@@ -1,30 +1,37 @@
 extern crate rand;
+extern crate clap;
 
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
+use clap::{Arg, App};
 
-use std::env;
 use std::process;
 
 const DEFAULT_LEN: usize = 16;
 
 fn main() {
-    let mut len: usize = DEFAULT_LEN;
+    let default_len = &DEFAULT_LEN.to_string();
+    let options = App::new("Random String Generator")
+        .version("0.1.0")
+        .arg(Arg::with_name("length").default_value(default_len))
+        .get_matches();
 
-    match env::args().nth(1) {
+    let len: usize;
+
+    match options.value_of("length") {
         Some(arg) => match arg.parse::<usize>() {
-            Ok(arg_len) => len = arg_len,
+            Ok(arg_len) => match arg_len {
+                0 => {
+                    len = DEFAULT_LEN;
+                }
+                _ => len = arg_len,
+            }
             _ => {
                 eprintln!("(len must be usize, got {})", arg);
                 process::abort();
             }
         },
-        _ => println!("(using default len={})", DEFAULT_LEN),
-    }
-
-    if 0 == len {
-        len = DEFAULT_LEN;
-        println!("(got len=0, using default len={})", len)
+        _ => len = DEFAULT_LEN,
     }
 
     let rand_string: String = thread_rng()

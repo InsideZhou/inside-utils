13e08ca7bf6d911a4e23a150ae7a6ca5ebe984c6
Inside <inside@insidezhou.me>
2021-10-20 00:30:24 +0800

    用rust实现majority-element-ii。

---
+++ rust/majority-element-ii/src/lib.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+pub fn majority_element_one_third(nums: Vec<usize>) -> Vec<usize> {
+    let one_third = nums.len() / 3;
+    let mut num_counts = HashMap::new();
+    for num in nums {
+        let count = num_counts.entry(num).or_insert(0usize);
+        *count += 1;
+    }
+
+    let mut result = num_counts.iter()
+        .filter(|(_, v)| **v > one_third)
+        .map(|(k, _)| *k)
+        .collect::<Vec<usize>>();
+
+    result.sort();
+    return result;
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_majority_element_one_third() {
+        assert_eq!(crate::majority_element_one_third(vec![1usize, 1, 2, 3, 1, 4, 5, 6, 1, 1]), [1usize]);
+
+        assert_eq!(crate::majority_element_one_third(vec![1usize, 1, 1, 1, 2, 2, 2, 2, 3, 1, 4, 2, 2, 1, 1]), [1usize, 2]);
+    }
+}
--- rust/path-in-zigzag-labelled-binary-tree/src/lib.rs
+++ rust/path-in-zigzag-labelled-binary-tree/src/lib.rs
@@ -1,54 +1,55 @@
+#[allow(dead_code)]
+pub fn path_in_zig_zag_tree(label: i32) -> Vec<i32> {
+    let path = generate_path(label as usize);
+    return path.iter().map(|l| *l as i32).collect();
+}
+
+pub fn generate_path(label: usize) -> Vec<usize> {
+    if 1 == label {
+        return vec![1];
+    }
+
+    let mut vec = vec![];
+    let mut l = label;
+    loop {
+        vec.insert(0, l);
+        l = get_parent_label(l);
+        if 1 == l {
+            vec.insert(0, 1);
+            break;
+        }
+    }
+
+    return vec;
+}
+
+pub fn count_used_bits(num: usize) -> usize {
+    let sizeof_usize = std::mem::size_of::<usize>();
+    let leading_zeros = (num as usize).leading_zeros() as usize;
+
+    return sizeof_usize * 8 - leading_zeros;
+}
+
+#[allow(dead_code)]
+fn get_row_separate_label(row: usize) -> usize {
+    let row_start = 2usize.pow(row as u32);
+    return row_start + row_start / 2;
+}
+
+fn get_parent_label(label: usize) -> usize {
+    let row = count_used_bits(label) - 1;
+    let row_start = 2usize.pow(row as u32);
+    let index_of_row = label - row_start;
+
+    let parent_row_start = 2usize.pow(row as u32 - 1);
+    let parent_index_max = row_start - parent_row_start - 1;
+    let parent_index = parent_index_max - index_of_row / 2;
+
+    return parent_row_start + parent_index;
+}
+
 #[cfg(test)]
-mod path_in_zigzag_labelled_binary_tree {
-    #[allow(dead_code)]
-    pub fn path_in_zig_zag_tree(label: i32) -> Vec<i32> {
-        let path = generate_path(label as usize);
-        return path.iter().map(|l| *l as i32).collect();
-    }
-
-    pub fn generate_path(label: usize) -> Vec<usize> {
-        if 1 == label {
-            return vec![1];
-        }
-
-        let mut vec = vec![];
-        let mut l = label;
-        loop {
-            vec.insert(0, l);
-            l = get_parent_label(l);
-            if 1 == l {
-                vec.insert(0, 1);
-                break;
-            }
-        }
-
-        return vec;
-    }
-
-    pub fn count_used_bits(num: usize) -> usize {
-        let sizeof_usize = std::mem::size_of::<usize>();
-        let leading_zeros = (num as usize).leading_zeros() as usize;
-
-        return sizeof_usize * 8 - leading_zeros;
-    }
-
-    fn get_row_separate_label(row: usize) -> usize {
-        let row_start = 2usize.pow(row as u32);
-        return row_start + row_start / 2;
-    }
-
-    fn get_parent_label(label: usize) -> usize {
-        let row = count_used_bits(label) - 1;
-        let row_start = 2usize.pow(row as u32);
-        let index_of_row = label - row_start;
-
-        let parent_row_start = 2usize.pow(row as u32 - 1);
-        let parent_index_max = row_start - parent_row_start - 1;
-        let parent_index = parent_index_max - index_of_row / 2;
-
-        return parent_row_start + parent_index;
-    }
-
+mod tests {
     use std::sync::{Once};
 
     static INIT: Once = Once::new();
@@ -64,62 +65,62 @@ mod path_in_zigzag_labelled_binary_tree {
     fn test_count_used_bits() {
         setup_test();
 
-        assert_eq!(count_used_bits(0b0000_0000_0000_0000_0000_0000_0000_0000), 0);
-        assert_eq!(count_used_bits(0b1111_1111_1111_1111_1111_1111_1111_1111), 32);
+        assert_eq!(crate::count_used_bits(0b0000_0000_0000_0000_0000_0000_0000_0000), 0);
+        assert_eq!(crate::count_used_bits(0b1111_1111_1111_1111_1111_1111_1111_1111), 32);
 
-        assert_eq!(count_used_bits(0b1111_1111), 8);
-        assert_eq!(count_used_bits(0b1101_1111), 8);
-        assert_eq!(count_used_bits(0b1101_1110), 8);
+        assert_eq!(crate::count_used_bits(0b1111_1111), 8);
+        assert_eq!(crate::count_used_bits(0b1101_1111), 8);
+        assert_eq!(crate::count_used_bits(0b1101_1110), 8);
 
-        assert_eq!(count_used_bits(0b0111_1111), 7);
-        assert_eq!(count_used_bits(0b0011_1111), 6);
-        assert_eq!(count_used_bits(0b0001_1111), 5);
+        assert_eq!(crate::count_used_bits(0b0111_1111), 7);
+        assert_eq!(crate::count_used_bits(0b0011_1111), 6);
+        assert_eq!(crate::count_used_bits(0b0001_1111), 5);
 
-        assert_eq!(count_used_bits(0b1111), 4);
-        assert_eq!(count_used_bits(0b0111), 3);
-        assert_eq!(count_used_bits(0b0011), 2);
-        assert_eq!(count_used_bits(0b0010), 2);
-        assert_eq!(count_used_bits(0b0001), 1);
+        assert_eq!(crate::count_used_bits(0b1111), 4);
+        assert_eq!(crate::count_used_bits(0b0111), 3);
+        assert_eq!(crate::count_used_bits(0b0011), 2);
+        assert_eq!(crate::count_used_bits(0b0010), 2);
+        assert_eq!(crate::count_used_bits(0b0001), 1);
     }
 
     #[test]
     fn test_get_row_separate_label() {
         setup_test();
 
-        assert_eq!(get_row_separate_label(2), 6);
-        assert_eq!(get_row_separate_label(3), 12);
-        assert_eq!(get_row_separate_label(5), 48);
+        assert_eq!(crate::get_row_separate_label(2), 6);
+        assert_eq!(crate::get_row_separate_label(3), 12);
+        assert_eq!(crate::get_row_separate_label(5), 48);
     }
 
     #[test]
     fn test_get_parent_label() {
         setup_test();
 
-        assert_eq!(get_parent_label(11), 6);
-        assert_eq!(get_parent_label(10), 6);
-        assert_eq!(get_parent_label(9), 7);
-        assert_eq!(get_parent_label(8), 7);
+        assert_eq!(crate::get_parent_label(11), 6);
+        assert_eq!(crate::get_parent_label(10), 6);
+        assert_eq!(crate::get_parent_label(9), 7);
+        assert_eq!(crate::get_parent_label(8), 7);
 
-        assert_eq!(get_parent_label(21), 13);
-        assert_eq!(get_parent_label(22), 12);
+        assert_eq!(crate::get_parent_label(21), 13);
+        assert_eq!(crate::get_parent_label(22), 12);
     }
 
     #[test]
     fn test_generate_path() {
         setup_test();
 
-        assert_eq!(generate_path(14), [1, 3, 4, 14]);
-        assert_eq!(generate_path(14), [1, 3, 4, 14]);
-        assert_eq!(generate_path(26), [1, 2, 6, 10, 26]);
+        assert_eq!(crate::generate_path(14), [1, 3, 4, 14]);
+        assert_eq!(crate::generate_path(14), [1, 3, 4, 14]);
+        assert_eq!(crate::generate_path(26), [1, 2, 6, 10, 26]);
 
-        assert_eq!(generate_path(1), [1]);
+        assert_eq!(crate::generate_path(1), [1]);
 
-        assert_eq!(generate_path(13), [1, 3, 5, 13]);
-        assert_eq!(generate_path(9), [1, 2, 7, 9]);
+        assert_eq!(crate::generate_path(13), [1, 3, 5, 13]);
+        assert_eq!(crate::generate_path(9), [1, 2, 7, 9]);
 
-        assert_eq!(generate_path(69), [1, 3, 4, 15, 17, 61, 69]);
+        assert_eq!(crate::generate_path(69), [1, 3, 4, 15, 17, 61, 69]);
 
-        let max_label_vec = generate_path(10usize.pow(6) as usize);
+        let max_label_vec = crate::generate_path(10usize.pow(6) as usize);
         let mut max_label_vec_str = vec![];
         for v in max_label_vec {
             max_label_vec_str.push(v.to_string())

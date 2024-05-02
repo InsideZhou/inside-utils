7bb58faa001c4ebaaa4cba9afb35328d3f872b57
Inside <inside@insidezhou.me>
2024-05-02 15:14:10 +0800

    重写“33. Search in Rotated Sorted Array”的二分查找。

--- python/leetcode/P33_search_in_rotated_sorted_array.py
+++ python/leetcode/P33_search_in_rotated_sorted_array.py
@@ -30,13 +30,52 @@ def search(nums: List[int], target: int) -> int:
     return -1
 
 
+def search_2(nums: List[int], target: int) -> int:
+    nums_length = len(nums)
+
+    if 1 == nums_length:
+        return -1 if nums[0] != target else 0
+
+    if nums[0] < nums[nums_length - 1]:
+        if target < nums[0] or target > nums[nums_length - 1]:
+            return -1
+        elif target == nums[0]:
+            return 0
+        elif target == nums[nums_length - 1]:
+            return nums_length - 1
+
+    split_index = nums_length // 2
+    left = search_2(nums[:split_index], target)
+    right = search_2(nums[split_index:], target)
+
+    if -1 == left and -1 == right:
+        return -1
+    elif -1 != left:
+        return left
+    elif -1 != right:
+        return right + split_index
+
+
 class TestSearchInRotatedSortedArray(unittest.TestCase):
-    def testBasic(self):
+    def testBasic1(self):
         self.assertEqual(5, search([4, 5, 6, 7, 0, 1, 2], 1))
+        self.assertEqual(5, search_2([4, 5, 6, 7, 0, 1, 2], 1))
+
+    def testBasic2(self):
         self.assertEqual(4, search([4, 5, 6, 7, 0, 1, 2], 0))
+        self.assertEqual(4, search_2([4, 5, 6, 7, 0, 1, 2], 0))
+
+    def testBasic3(self):
         self.assertEqual(-1, search([4, 5, 6, 7, 0, 1, 2], 3))
+        self.assertEqual(-1, search_2([4, 5, 6, 7, 0, 1, 2], 3))
+
+    def testBasic4(self):
         self.assertEqual(-1, search([1], 0))
+        self.assertEqual(-1, search_2([1], 0))
+
+    def testBasic5(self):
         self.assertEqual(0, search([1, 3, 5], 1))
+        self.assertEqual(0, search_2([1, 3, 5], 1))
 
 
 if __name__ == '__main__':

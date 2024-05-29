d55c2338a59f0319fc04da13cbeec4e75c8b10a4
Inside <inside@insidezhou.me>
2024-05-29 20:26:06 +0800

    给“80. Remove Duplicates from Sorted Array II”添加注释。

--- python/leetcode/P80_remove_duplicates_from_sorted_array_ii.py
+++ python/leetcode/P80_remove_duplicates_from_sorted_array_ii.py
@@ -9,6 +9,11 @@ MAX_DUPLICATION_LENGTH = 2
 
 
 def remove_duplicates(nums: List[int]) -> int:
+    """
+    利用集合是有序的特性，双指针遍历该集合，主指针确定重复元素的七点，副指针确定重复元素的终点。
+    时间复杂度是O(n)，空间复杂度是O(1)。
+    """
+
     duplication_start = 0
 
     while duplication_start < len(nums):

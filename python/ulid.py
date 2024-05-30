1129404e9a26c8fee6b0459c9676f9778428ec8d
Inside <inside@insidezhou.me>
2024-05-30 22:08:11 +0800

    完善ulid实现。

--- python/ulid.py
+++ python/ulid.py
@@ -14,10 +14,10 @@ class ULID:
     """
     Inspired by https://github.com/ulid/spec.
     Main feature is distributed and sortable.
-    Use 32-based number system, [0-9a-v], 5 bits per character, case-insensitive
+    Use 32-based number system, [0-9] and alphabet without 'i l o u', 5 bits per character, case-insensitive
     Format: "timestamp_bits-worker_bits-random_bits"
-    Random_bits are default to 48-bit, worker_bits are default to 16-bit, the rest are timestamp bits.
-    ULID bits are limited by N-bit computer, but under uuid compatibility, they're 128 bits.
+    Random_bits default to 48-bit, worker_bits default to 16-bit, the rest are timestamp bits.
+    ULID bits are limited by N-bit Computer, under uuid compatibility, they're 128 bits.
     """
 
     digits = string.digits + string.ascii_lowercase[:22]
@@ -40,8 +40,8 @@ class ULID:
             raise ValueError(f"Unrecognized {ulid=}")
 
         new_id = ULID(0)
-        new_id.timestamp_millis, new_id.worker_id, new_id.rand_num = (ULID.unsigned_int_from_32base(x)
-                                                                      for x in matches.group(1, 2, 3))
+        new_id.timestamp, new_id.worker_id, new_id.rand_num = (ULID.unsigned_int_from_32base(x)
+                                                               for x in matches.group(1, 2, 3))
 
         return new_id
 
@@ -56,7 +56,7 @@ class ULID:
         new_id.worker_id = int(uuid_bits[-ULID.worker_bits:], 2)
 
         uuid_bits = uuid_bits[:-ULID.worker_bits]
-        new_id.timestamp_millis = int(uuid_bits, 2)
+        new_id.timestamp = int(uuid_bits, 2)
 
         return new_id
 
@@ -80,19 +80,19 @@ class ULID:
         base = ULID.mask + 1
         return sum([ULID.digits.index(v) * pow(base, i) for i, v in enumerate(txt.lower()[::-1])])
 
-    def __init__(self, worker_id, rand: random.Random = None):
+    def __init__(self, worker_id, timestamp: int = None, rand: random.Random = None):
         if worker_id < 0:
             raise ValueError(f"{worker_id=} should not be negative")
 
         self.worker_id = worker_id
-        self.timestamp_millis = int(time.time() * 1000)
+        self.timestamp = timestamp or int(time.time() * 1000)
         self.rand_num = (rand or ULID.rand).randint(0, pow(2, ULID.rand_bits) - 1)
 
     def __repr__(self):
-        return f"ULID({self.worker_id})"
+        return f"ULID({self.worker_id}, {self.timestamp}, {self.rand_num})"
 
     def __str__(self):
-        ts = ULID.unsigned_int_to_32base(self.timestamp_millis).rjust(ULID.__timestamp_length, "0")
+        ts = ULID.unsigned_int_to_32base(self.timestamp).rjust(ULID.__timestamp_length, "0")
         worker = ULID.unsigned_int_to_32base(self.worker_id).rjust(ULID.__worker_length, '0')
         r = ULID.unsigned_int_to_32base(self.rand_num).rjust(ULID.__rand_length, '0')
 
@@ -100,29 +100,29 @@ class ULID:
 
     def __eq__(self, other):
         return isinstance(other, ULID) \
-            and self.timestamp_millis == other.timestamp_millis \
+            and self.timestamp == other.timestamp \
             and self.worker_id == other.worker_id \
             and self.rand_num == other.rand_num
 
     def uuid(self) -> uuid.UUID:
         rand_bytes = self.rand_num.to_bytes(ULID.rand_bits // 8, byteorder="big")
         worker_bytes = self.worker_id.to_bytes(ULID.worker_bits // 8, byteorder="big")
-        ts_bytes = self.timestamp_millis.to_bytes(ULID.timestamp_bits // 8, byteorder="big")
+        ts_bytes = self.timestamp.to_bytes(ULID.timestamp_bits // 8, byteorder="big")
 
         return uuid.UUID(ts_bytes.hex() + worker_bytes.hex() + rand_bytes.hex())
 
     def value(self) -> str:
-        ts = ULID.unsigned_int_to_32base(self.timestamp_millis)
-        worker = ULID.unsigned_int_to_32base(self.worker_id)
-        r = ULID.unsigned_int_to_32base(self.rand_num)
+        ts = ULID.unsigned_int_to_32base(self.timestamp)
+        worker = ULID.unsigned_int_to_32base(self.worker_id).rjust(ULID.__worker_length, '0')
+        r = ULID.unsigned_int_to_32base(self.rand_num).rjust(ULID.__rand_length, '0')
 
         return f"{ts}{worker}{r}"
 
     def bin_value(self) -> str:
-        return f"{self.timestamp_millis:b}{self.worker_id:b}{self.rand_num:b}"
+        return f"{self.timestamp:b}{self.worker_id:b}{self.rand_num:b}"
 
     def hex_value(self) -> str:
-        return f"{self.timestamp_millis:X}{self.worker_id:X}{self.rand_num:X}"
+        return f"{self.timestamp:X}{self.worker_id:X}{self.rand_num:X}"
 
     def bytes(self, byteorder="big") -> bytes:
         val = self.hex_value()
@@ -149,11 +149,11 @@ class TestULID(unittest.TestCase):
         self.assertEqual(["a", "ab"], arr)
 
         ulid = ULID(0)
-        print(ulid, "|", ulid.uuid(), "|", ulid.value(), "|", int(ulid.value(), 32), "|", ulid.hex_value(), "|",
-              ulid.bytes())
+        print(ulid, f"| uuid={ulid.uuid()}", f"| value={ulid.value()}", f"| int={int(ulid.value(), 32)}",
+              f"| hex={ulid.hex_value()}", f"| bytes={ulid.bytes()}")
 
         ulid = ULID(random.randint(0, 65535))
-        print(ulid, "|", ulid.uuid(), "|", ulid.value())
+        print(ulid, f"| uuid={ulid.uuid()}", f"| value={ulid.value()}")
 
     def testPerformance(self):
         worker_id = random.randint(0, 65535)
@@ -165,7 +165,7 @@ class TestULID(unittest.TestCase):
 
         elapsed = (time.time_ns() - start) / 1000000
 
-        print(f"{elapsed:.3f}ms elapsed for generating {turns} ULID, {turns / (elapsed / 1000):.0f}id/s.")
+        print(f"{elapsed:.3f}ms elapsed for generating {turns} ULID, {turns / (elapsed / 1000):.0f} id/s.")
 
     def testUnsignedIntFrom32base(self):
         self.assertEqual(15000, ULID.unsigned_int_from_32base("eko"))

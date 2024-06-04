37e9a961bd3350c01fa31670d9202ca94177e122
Inside <inside@insidezhou.me>
2024-06-04 15:37:48 +0800

    为“120. Triangle”添加注释。

--- python/leetcode/P120_triangle.py
+++ python/leetcode/P120_triangle.py
@@ -5,15 +5,16 @@ from typing import List
 
 
 def minimum_total(triangle: List[List[int]]) -> int:
-    row: List[int] = []
+    """
+    在第N行时，该行每一个元素都表示一条或多条可能的路径，要得到值最小的路径，必须是当前行每个元素与前一行的路径值相加，其中最小的就是当前的最小路径。
+    """
 
-    for i in range(len(triangle)):
+    row: List[int] = triangle[0]
+
+    for i in range(1, len(triangle)):
         upper = row
         row = triangle[i]
         row_length = len(row)
-
-        if 0 == i:
-            continue
 
         for j in range(row_length):
             if 0 == j:
